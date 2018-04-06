002448de5c0903a6a4a8318bd8474ce1a53bf51a
Jonas Folkvord Triki <jonas.triki@student.uib.no>
2018-04-06 19:33:06 +0200

    api

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", init);
+
+let matches;
+let resultsDiv;
+
+function init() {
+
+    // Retrive DOM
+    resultsDiv = document.querySelector("#results");
+
+    // Load matches from API.
+    fetch("http://api.football-data.org/v1/fixtures")
+        .then(res => res.json())
+        .then(populateList)
+        .catch(console.log);
+}
+
+function populateList(data) {
+    matches = data;
+    console.log(matches);
+    
+    resultsDiv.innerHTML = "";
+}
+
+function filter(query, matches) {
+    if (query === "") return matches;
+    
+}
